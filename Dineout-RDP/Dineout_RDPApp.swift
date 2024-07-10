ef9466f2c8d9122583a64c99c625f5bdaefe019d
Naman-patel77 <naman.patel_int@external.swiggy.in>
2024-07-10 12:36:06 +0530

    Initial Commit

---
+++ Dineout-RDP/Dineout_RDPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dineout_RDPApp.swift
+//  Dineout-RDP
+//
+//  Created by naman.patel_int on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Dineout_RDPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
